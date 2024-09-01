9543bcff78631f37ba5f0168b12cd8279a5c71e2
Mahmud <mahmudimranemu@gmail.com>
2024-09-02 00:51:45 +0600

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import { timeStamp } from "console";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamp: true }
+);
+
+const User = mongoose.Model("User", userSchema);
+
+export default User;
