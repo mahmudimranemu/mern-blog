3c742d7f70fa75e9d3bcaa6d613de3ac02ed67dd
Mahmud <mahmudimranemu@gmail.com>
2024-09-01 22:24:14 +0600

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,40 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingIn from "./pages/SignIn";
+import SingUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/about'
+          element={<About />}
+        />
+        <Route
+          path='/signin'
+          element={<SingIn />}
+        />
+        <Route
+          path='/signup'
+          element={<SingUp />}
+        />
+        <Route
+          path='/dashboard'
+          element={<Dashboard />}
+        />
+        <Route
+          path='/projects'
+          element={<Projects />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
